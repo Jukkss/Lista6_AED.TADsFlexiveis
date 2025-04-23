1137ad2305b645344a8768411398bb45af9b3ea9
João Victor Soares Souza <sjuao7729@gmail.com>
2025-04-22 23:20:28 -0300

    Questão 2, 3 (Verde)

---
+++ Questão2/Celula-Pilha-Main2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão2
+{
+    internal class Program
+    {
+        /*Celula*/
+        class Celula
+        {
+            private int elemento;
+            private Celula prox;
+
+            public Celula(int elemento)
+            {
+                this.elemento = elemento;
+                this.prox = null;
+            }
+
+            public Celula()
+            {
+                this.elemento = 0;
+                this.prox = null;
+            }
+
+            public Celula Prox
+            {
+                get { return this.prox; }
+                set { this.prox = value; }
+            }
+
+            public int Elemento
+            {
+                get { return this.elemento; }
+                set { this.elemento = value; }
+            }
+        }
+
+        /*Pilha*/
+        class Pilha 
+        {
+            private Celula topo;
+
+            public Pilha()
+            {
+                topo = null;
+            }
+            public void Inserir(int num)
+            {
+                Celula tmp = new Celula(num);
+                tmp.Prox = topo;
+                topo = tmp;
+            }
+            public int Remover()
+            {
+                if (topo == null)
+                {
+                    throw new Exception("Erro!");
+                }
+
+                int elemento = topo.Elemento;
+                topo = topo.Prox;
+                return elemento;
+            }
+
+            /*Main*/
+            public bool VrfVazia()
+            {
+                return topo == null;
+            }
+            static void Main(string[] args)
+            {
+                string linha;
+                while (!string.IsNullOrEmpty(linha = Console.ReadLine()))
+                {
+                    int numero = int.Parse(linha);
+                    Pilha pilha = new Pilha();
+
+                    if (numero == 0)
+                    {
+                        Console.WriteLine(0);
+                        continue;
+                    }
+
+                    while (numero > 0)
+                    {
+                        int resto = numero % 8;
+                        pilha.Inserir(resto);
+                        numero /= 8;
+                    }
+                    while (!pilha.VrfVazia())
+                    {
+                        Console.Write(pilha.Remover());
+                    }
+                    Console.WriteLine(); 
+                }
+            }
+        }
+    }
+}
---
+++ Questão3/Arquivo-Celula-Fila-Main.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão3
+{
+    /*Arquivo*/
+    class Arquivo
+    {
+        public string Nome { get; set; }
+        public int Paginas { get; set; }
+
+        public Arquivo(string nome, int paginas)
+        {
+            Nome = nome;
+            Paginas = paginas;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} {Paginas}";
+        }
+    }
+    
+    /*Celula*/
+    class Celula
+    {
+        public Arquivo Elemento { get; set; }
+        public Celula Prox { get; set; }
+
+        public Celula(Arquivo elemento)
+        {
+            this.Elemento = elemento;
+            this.Prox = null;
+        }
+    }
+
+    /*Fila*/
+    class Fila
+    {
+        private Celula primeiro;
+        private Celula ultimo;
+
+        public Fila()
+        {
+            primeiro = ultimo = null;
+        }
+        public void Inserir(Arquivo arquivo)
+        {
+            Celula nova = new Celula(arquivo);
+            if (primeiro == null)
+            {
+                primeiro = ultimo = nova;
+            }
+            else
+            {
+                ultimo.Prox = nova;
+                ultimo = nova;
+            }
+        }
+        public Arquivo Remover()
+        {
+            if (primeiro == null)
+                throw new Exception("Fila de impressão vazia");
+
+            Arquivo removido = primeiro.Elemento;
+            primeiro = primeiro.Prox;
+            if (primeiro == null)
+                ultimo = null;
+
+            return removido;
+        }
+        public void Mostrar()
+        {
+            for (Celula i = primeiro; i != null; i = i.Prox)
+            {
+                Console.WriteLine(i.Elemento.ToString());
+            }
+        }
+        public bool VrfVazia()
+        {
+            return primeiro == null;
+        }
+
+        /*Main*/
+        static void Main(string[] args)
+        {
+            Fila fila = new Fila();
+            int opcao;
+
+            do
+            {
+                Console.WriteLine("Op:");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        string nome = Console.ReadLine();
+                        int paginas = int.Parse(Console.ReadLine());
+                        Arquivo arquivo = new Arquivo(nome, paginas);
+                        fila.Inserir(arquivo);
+                        break;
+
+                    case 2:
+                        if (fila.VrfVazia())
+                        {
+                            Console.WriteLine("Fila de impressão vazia");
+                        }
+                        else
+                        {
+                            Arquivo impresso = fila.Remover();
+                            Console.WriteLine(impresso.Nome);
+                        }
+                        break;
+
+                    case 3:
+                        fila.Mostrar();
+                        break;
+
+                    case 4:
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+            } while (opcao != 4);
+        }
+    }
+}
